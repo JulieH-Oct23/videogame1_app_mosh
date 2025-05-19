788ae4b1c46c07b3bf0af28562d3baf09a855d17
Julie Hart <147504798+JulieH-Oct23@users.noreply.github.com>
2025-05-18 20:06:01 -0600

    update to mosh ed

---
+++ src/components/ListGroup/ListGroup.tsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import "./ListGroup.css";
+import styled from "styled-components";
+import { IoHeart } from "react-icons/io5";
+
+const List = styled.ul`
+  list-style: none;
+  padding: 0;
+`;
+
+// const Love = () => {
+//   return <IoHeart color="salmon" size="70" />;
+// };
+
+interface ListItemProps {
+  active: boolean;
+}
+
+const ListItem = styled.li<ListItemProps>`
+  padding: 5px;
+  background: ${(props) => (props.active ? "salmon" : "none")};
+`;
+
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  return (
+    <>
+      <h1>{heading}</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <List>
+        {items.map((item, index) => (
+          <ListItem
+            active={index === selectedIndex}
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </ListItem>
+        ))}
+      </List>
+    </>
+  );
+}
+
+export default ListGroup;
+
+// export default Love;
